8a8b29cce5ecad04004a9eaf88b36500c2ac0b1a
Adrià Company Pastor <adricom2006@gmail.com>
2026-01-14 19:51:58 +0100

    Eliminar archivos inecesarios

--- custom_addons/pi_core/models/__init__.py
+++ custom_addons/pi_core/models/__init__.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 
-# from . import pi_categoria
-# from . import pi_comentario
-# from . import pi_compra
-# from . import pi_producto
-# from . import pi_reporte
-# from . import pi_usuarios
-# from . import pi_valoracion
-# from . import pi_producto_imagen
-# from . import pi_etiqueta
+from . import pi_categoria
+from . import pi_comentario
+from . import pi_compra
+from . import pi_producto
+from . import pi_reporte
+from . import pi_usuarios
+from . import pi_valoracion
+from . import pi_producto_imagen
+from . import pi_etiqueta
