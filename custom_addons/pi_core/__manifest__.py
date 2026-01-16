48b1d603414ba5db46df89cad481a876ce25491c
Adrià Company Pastor <adricom2006@gmail.com>
2026-01-16 19:02:37 +0100

    Se han elminiado comentarios sobrantes.

--- custom_addons/pi_core/__manifest__.py
+++ custom_addons/pi_core/__manifest__.py
@@ -11,16 +11,11 @@ Long description of module's purpose
     'author': "My Company",
     'website': "https://www.yourcompany.com",
 
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
     'category': 'Uncategorized',
     'version': '0.1',
 
-    # any module necessary for this one to work correctly
     'depends': ['base','mail','web'],
 
-    # always loaded
     'data': [
         'data/ir_sequence.xml',
         'security/ir.model.access.csv',
@@ -34,7 +29,6 @@ Long description of module's purpose
         'views/pi_compra_views.xml',
         'views/pi_reporte_views.xml',
     ],
-    # only loaded in demonstration mode
     'demo': [
         'demo/demo.xml',
     ],
